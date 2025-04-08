d2fe53c46cc58a2c57e97204f8e52db6c61f3541
CPPGhettoIrony <manolitor2003@gmail.com>
2025-04-08 13:09:14 +0200

    master

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,9 +11,10 @@ public class Player : MonoBehaviour
 
     Vector2 debugCollision = Vector2.zero;
 
-    bool isGrounded = false, isJumping = false, isCrouching = false, isSprinting = false;
+    bool isGrounded = false, isJumping = false, canWallJump = false, isCrouching = false, isSprinting = false;
     float jumpCounter = 0, movement, proxSpeed, hspeed = 0;
-    private float speed = 10, jumpMax = 15;
+    float speed = 10, jumpMax = 15;
+    int face = 1;
 
     void Start()
     {
@@ -31,6 +33,7 @@ public class Player : MonoBehaviour
         proxSpeed = movement * speed;
 
         Debug.DrawLine(rb.transform.position, debugCollision, Color.magenta);
+        Debug.DrawLine(rb.transform.position, rb.transform.position + Vector3.left * capsule.size.x/2, Color.red);
     }
 
     void FixedUpdate()
@@ -41,13 +44,15 @@ public class Player : MonoBehaviour
             if(jumpCounter>=jumpMax) isJumping = false;
         } 
 
-        if(rb.linearVelocityX != hspeed && rb.linearVelocityX == 0) {
+        if(rb.linearVelocityX != hspeed && rb.linearVelocityX == 0 && !canWallJump) {
             hspeed = 0;
-            Debug.Log("Cant Move");
+            canWallJump = Mathf.Abs(rb.linearVelocityY) > 0.1;
         }
 
                 if(proxSpeed > rb.linearVelocityX)  hspeed += 0.5f;
         else    if(proxSpeed < rb.linearVelocityX)  hspeed -= 0.5f;
+
+        //Debug.Log(hspeed);
 
         rb.linearVelocityX = hspeed;
     }
@@ -57,10 +62,13 @@ public class Player : MonoBehaviour
         if(collision.gameObject.layer == 3) {
             isGrounded = true; 
             foreach (ContactPoint2D point in collision.contacts) {
-                Debug.Log("" + point.point.y + ", " + rb.transform.position.y);
-                if(point.point.y >= rb.transform.position.y - capsule.size.y)  {
+                //Debug.Log("" + point.point.y + ", " + rb.transform.position.y);
+                if(point.point.y >= rb.transform.position.y + capsule.size.y * 0.8)
+                    isJumping = false;
+                if(point.point.y >= rb.transform.position.y) {
                     debugCollision = point.point;
-                } 
+                    canWallJump = false;
+                }
             }
         }
     }
@@ -74,11 +82,17 @@ public class Player : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context) {
         movement = context.ReadValue<Vector2>().x;
+        if(movement != 0)
+            face = (int)(movement/Math.Abs(movement));
     }
 
     public void Jump(InputAction.CallbackContext context) {
         if(isGrounded && context.performed) {
             isJumping = true;
+            if(canWallJump) {
+                hspeed = -face * speed;
+                Debug.Log("walljump: " + (-face) + " " + hspeed);
+            }
             jumpCounter = 0;
         } if(context.canceled)
             isJumping = false;
