dac9f7cb6d6a17cdc408860be46382b7f758b083
CPPGhettoIrony <manolitor2003@gmail.com>
2025-04-23 21:26:56 +0200

    master

--- Assets/Scripts/qBlock.cs
+++ Assets/Scripts/qBlock.cs
@@ -4,16 +4,16 @@ public class qBlock : MonoBehaviour, Block
 {
     Vector3 currentPos, hitPos;
     Animator animator;
-    BoxCollider2D collider;
+    BoxCollider2D boxcollider;
     bool isFull = true;
 
     public GameObject spawn;
     void Start()
     {
-        currentPos = transform.position; 
-        hitPos = transform.position + Vector3.up * 0.25f;
-        collider = GetComponent<BoxCollider2D>();
-        animator = GetComponent<Animator>();
+        currentPos  = transform.position; 
+        hitPos      = transform.position + Vector3.up * 0.25f;
+        boxcollider = GetComponent<BoxCollider2D>();
+        animator    = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,7 +27,7 @@ public class qBlock : MonoBehaviour, Block
             isFull = false;
             transform.position = hitPos;
             animator.SetTrigger("hit");
-            Vector3 pos = transform.position + new Vector3(collider.bounds.size.x/2, collider.bounds.size.y/2, 0f);
+            Vector3 pos = transform.position + new Vector3(boxcollider.bounds.size.x/2, boxcollider.bounds.size.y/2, 0f);
             Instantiate(spawn, pos, Quaternion.identity);
         }
     }
